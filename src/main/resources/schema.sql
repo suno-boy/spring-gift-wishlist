25ee37cc211b465599960816a054aa44cbf5c6c3
suno-boy <tnsgh3691@gmail.com>
2024-07-05 16:45:02 +0900

    docs: 테이블 이름 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,9 +5,8 @@ CREATE TABLE Product (
                          imageUrl VARCHAR(1024) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS UserEntity (
-                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                    email VARCHAR(255) NOT NULL UNIQUE,
-                    password VARCHAR(255) NOT NULL,
-                    UNIQUE (email)
-);
+CREATE TABLE IF NOT EXISTS users (
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        email VARCHAR(255) NOT NULL UNIQUE,
+                        password VARCHAR(255) NOT NULL
+;
