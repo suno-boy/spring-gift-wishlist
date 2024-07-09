74d9e3b29595a3d22b4633a5b00bd99a75313ed7
suno-boy <tnsgh3691@gmail.com>
2024-07-10 01:06:13 +0900

    feat: 스키마에 wish테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS users (
                         email VARCHAR(255) NOT NULL UNIQUE,
                         password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE wishes
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT,
+    product_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
